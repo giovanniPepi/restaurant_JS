d20d7ceea8914b4bb1eb7bcafe292c8d320b4d51
gtpepi <gtpepi@hotmail.com>
2022-04-19 11:07:56 -0300

    Add base development structure

---
+++ main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('it\'s working!');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLDhCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudF9qcy8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnaXRcXCdzIHdvcmtpbmchJyk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
