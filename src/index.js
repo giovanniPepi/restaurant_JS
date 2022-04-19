cc1b2a84f940f59deebdafc21c96d6530c825f15
gtpepi <gtpepi@hotmail.com>
2022-04-19 14:20:10 -0300

    Move header cration to JS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
 import './style.css';
+// asset for logo 
 import avatar from './img/GitHub-Mark-64px.png';
-
 
 // import other modules from different .js here
 
-const logo = document.querySelector('.logo');
-const myIcon = new Image();
-myIcon.src = avatar;
-myIcon.setAttribute('id', 'logo');
+const dQuery = (() => {
+  
+  const logo = document.querySelector('.logo');
+  const git = new Image();  
+  git.src = avatar;
+  git.setAttribute('id', 'logo');
+  
+  logo.appendChild(git);
 
-logo.appendChild(myIcon);
+  // HTML base skeleton creator
+  const container = document.querySelector('.container');
+
+  const header = document.createElement('div');
+  header.setAttribute('class', 'header');
+
+  const tabs = document.createElement('div');
+  tabs.setAttribute('class', 'tabs');
+
+  const home = document.createElement('div');
+  home.setAttribute('class', 'home');
+  home.textContent = 'Home';
+
+  const menu = document.createElement('div');
+  menu.setAttribute('class', 'menu');
+  menu.textContent = 'Menu';
+  
+  const contact = document.createElement('div');
+  contact.setAttribute('class', 'contact');
+  contact.textContent = 'Contact';
+
+  tabs.appendChild(home);
+  tabs.appendChild(menu);
+  tabs.appendChild(contact);
+
+  header.appendChild(tabs);
+  
+  container.prepend(header);
+
+
+
+  //Home main menu creator
+
+  const main = document.querySelector('.main');
+  const para1 = document.createElement('p');
+  para1.textContent = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae eius quis ad omnis maiores provident ullam officia dolorem molestiae ratione rem cumque fuga voluptatibus vel, est minima necessitatibus hic quam?";
+
+  main.appendChild(para1);
+
+
+})();
