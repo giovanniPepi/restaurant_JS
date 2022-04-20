f07e575a102b64c394299f7f6d26598334c55b25
gtpepi <gtpepi@hotmail.com>
2022-04-20 16:56:49 -0300

    Update margins of contact tab

--- src/contactLoader.js
+++ src/contactLoader.js
@@ -1,4 +1,6 @@
 import clear from "./clear";
+import mapImg from "./img/map.png"
+import instIcon from "./img/inst.png"
 
 const contactLoader = () => {
   clear();
@@ -6,12 +8,54 @@ const contactLoader = () => {
   const container = document.querySelector('.container');
   const footer = document.querySelector(".footer");
 
-  const para1 = document.createElement('p');
+  //creation
   const menuContainer = document.createElement('div'); 
   menuContainer.setAttribute('class', 'menuContainer');
-  para1.textContent = "contactLorem ipsum, dolor sit amet consectetur adipisicing elit. Quae eius quis ad omnis maiores provident ullam officia dolorem molestiae ratione rem cumque fuga voluptatibus vel, est minima necessitatibus hic quam?";
+
+  const para1 = document.createElement('p');
+  para1.setAttribute('class', 'para1');
+  para1.textContent = "Contact Us";
+
+  const para2 = document.createElement('p');
+  para2.setAttribute('class', 'para2');
+  para2.textContent = "Location"; 
+
+  const para2Desc = document.createElement('p');
+  para2Desc.setAttribute('class', 'para2Desc');
+  para2Desc.textContent = "Via Santa Maria, 119, Pisa, IT "
+  
+  const para2Desc2 = document.createElement('div');
+  para2Desc2.setAttribute('class', 'para2Desc2');
+  
+  const inst = new Image();
+  inst.src = instIcon;
+  inst.setAttribute('class', 'flatIcon');
+  
+  const para2Desc2Inst = document.createElement("p");
+  para2Desc2Inst.setAttribute('class', 'inst');
+  para2Desc2Inst.textContent = "@pizzeriaaa";  
+
+  const para2Desc3 = document.createElement('p');
+  para2Desc3.setAttribute('class', 'para2Desc3');
+  para2Desc3.textContent = "📞 49 6568 7835";
+  
+  
+  const map = new Image(); 
+  map.src = mapImg;
+  map.setAttribute("class", "mapImg");
+
+
+  //appends
+  para2Desc2.appendChild(inst);
+  para2Desc2.appendChild(para2Desc2Inst);
+
+  para2.appendChild(para2Desc);
+  para2.appendChild(para2Desc2);
+  para2.appendChild(para2Desc3);
 
   menuContainer.appendChild(para1);
+  menuContainer.appendChild(para2);
+  menuContainer.appendChild(map);
   main.appendChild(menuContainer);
 
   container.insertBefore(main, footer);
