033ae90013629203c1c33cca7e66ebee452d0b25
gtpepi <gtpepi@hotmail.com>
2022-04-20 15:06:14 -0300

    Update style for menuLoader, update menuloader creation

--- src/menuTabLoader.js
+++ src/menuTabLoader.js
@@ -6,8 +6,6 @@ import pizza4 from "./img/pexels-engin-akyurt-2619970.jpg";
 import pizza5 from "./img/pexels-kristina-paukshtite-1146760.jpg";
 import pizza6 from "./img/pexels-narda-yescas-1566837.jpg";
 
-
-
 const menuTabLoader = () => {
   clear();
   const main = document.querySelector('.main');
@@ -25,67 +23,70 @@ const menuTabLoader = () => {
   const menuGrid = document.createElement('div');
   menuGrid.setAttribute('class', 'menuGrid');
 
-
   //grid items
   const pizz1 = document.createElement('div');
+  pizz1.setAttribute('class', 'pizzaItem');
   
   const pizz1Img = new Image();
   pizz1Img.src = pizza1;
-  pizz1Img.setAttribute('class', 'pizzaItem');
+  pizz1Img.setAttribute('class', 'pizzaItemImg');
 
   const pizz1Desc = document.createElement('p');
   pizz1Desc.textContent = 'le pizze';
 
   const pizz2 = document.createElement('div');
+  pizz2.setAttribute('class', 'pizzaItem');
 
   const pizz2Img = new Image();
   pizz2Img.src = pizza2;
-  pizz2Img.setAttribute('class', 'pizzaItem');
+  pizz2Img.setAttribute('class', 'pizzaItemImg');
 
   const pizz2Desc = document.createElement('p');
   pizz2Desc.textContent = 'le pizze2';
 
   const pizz3 = document.createElement('div');
+  pizz3.setAttribute('class', 'pizzaItem');
 
   const pizz3Img = new Image();
   pizz3Img.src = pizza3;
-  pizz3Img.setAttribute('class', 'pizzaItem');
+  pizz3Img.setAttribute('class', 'pizzaItemImg');
 
   const pizz3Desc = document.createElement('p');
   pizz3Desc.textContent = 'le pizze3';
 
   const pizz4 = document.createElement('div');
+  pizz4.setAttribute('class', 'pizzaItem');
 
   const pizz4Img = new Image();
   pizz4Img.src = pizza4;
-  pizz4Img.setAttribute('class', 'pizzaItem');
+  pizz4Img.setAttribute('class', 'pizzaItemImg');
 
   const pizz4Desc = document.createElement('p');
   pizz4Desc.textContent = 'le pizze4';
 
   const pizz5 = document.createElement('div');
+  pizz5.setAttribute('class', 'pizzaItem');
 
   const pizz5Img = new Image();
-  pizz5Img.src = pizza4;
-  pizz5Img.setAttribute('class', 'pizzaItem');
+  pizz5Img.src = pizza5;
+  pizz5Img.setAttribute('class', 'pizzaItemImg');
 
   const pizz5Desc = document.createElement('p');
   pizz5Desc.textContent = 'le pizze5';
 
   const pizz6 = document.createElement('div');
+  pizz6.setAttribute('class', 'pizzaItem');
 
   const pizz6Img = new Image();
   pizz6Img.src = pizza6;
-  pizz6Img.setAttribute('class', 'pizzaItem');
+  pizz6Img.setAttribute('class', 'pizzaItemImg');
 
   const pizz6Desc = document.createElement('p');
   pizz6Desc.textContent = 'le pizze6';
 
-
-
-
-
-
+  const para5 = document.createElement('p');
+  para5.setAttribute('class', 'para5');
+  para5.textContent = "All pizzas are produced with natural fermentation";
 
   //appends
 
@@ -117,9 +118,10 @@ const menuTabLoader = () => {
   menuGrid.appendChild(pizz5);
   menuGrid.appendChild(pizz6);
 
-
   menuContainer.appendChild(para1);
   menuContainer.appendChild(menuGrid);
+  menuContainer.appendChild(para5);
+
   main.appendChild(menuContainer);
 
   container.insertBefore(main, footer);
