bab50d3f27b195881c10fb86207c41f291680007
gtpepi <gtpepi@hotmail.com>
2022-04-20 22:30:38 -0300

    Fix directories

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './style.css';
 import dQuery from './initialLoader.js';
+import clear from './clear';
+import contactLoader from './contactLoader';
+import getListeners from './getListeners';
 import menuLoader from './menuLoader';
+import menuTabLoader from './menuTabLoader';
 
 // calls other modules, add eventListeners
 const main = (() => {
