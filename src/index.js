2e17ecd6e3c4bcba8ef761c933270c6f69614380
gtpepi <gtpepi@hotmail.com>
2022-04-20 21:40:59 -0300

    Update width. Update Names. Update paddings

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './style.css';
 import dQuery from './initialLoader.js';
+import menuLoader from './menuLoader';
 
 // calls other modules, add eventListeners
 const main = (() => {
   dQuery(); 
+  menuLoader('home');
 })();
 
 
